b1afbdff760dbaa4ee7b1db1850c1e9d2e952f76
derciojds <derciojdsd@gmail.com>
2023-07-17 20:55:28 +0200

    Adicionar componente NavBar no header

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
+import { NavBar } from "./components/nav-bar";
+
 export default function App() {
     return (
-        <div className="container">
-            <h1>Teste</h1>
-        </div>
+        <>
+            <header>
+                <NavBar />
+            </header>
+            <main className="container">
+                <h1>Teste</h1>
+            </main>
+        </>
     );
 }
