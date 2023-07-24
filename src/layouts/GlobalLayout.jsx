9bd3a7fa29b45b468f94b615a7150a20e143e580
derciojds <derciojdsd@gmail.com>
2023-07-24 17:14:58 +0200

    refactor: Adicionar Header no layout global

--- src/layouts/GlobalLayout.jsx
+++ src/layouts/GlobalLayout.jsx
@@ -1,10 +1,13 @@
-import { Outlet } from "react-router-dom";
+import { Outlet } from 'react-router-dom'
+import { NavBar } from '../components/nav-bar'
 
 export default function GlobalLayout() {
     return (
         <>
-            <h1>layout global</h1>
+            <header>
+                <NavBar />
+            </header>
             <Outlet />
         </>
-    );
+    )
 }
