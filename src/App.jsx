2b69a7aad67b815ae985b50ec0669f94d6bbe0de
derciojds <derciojdsd@gmail.com>
2023-07-24 17:29:46 +0200

    fix: Renderizar comp. Home invés de countries

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import GlobalLayout from "./layouts/GlobalLayout";
-import Countries from "./pages/Countries";
-import Country from "./pages/Country";
-import ErrorPage from "./pages/ErrorPage";
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import GlobalLayout from './layouts/GlobalLayout'
+import Country from './pages/Country'
+import ErrorPage from './pages/ErrorPage'
+import { Home } from './pages/Home/'
 
 export default function App() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<GlobalLayout />}>
-                    <Route index element={<Countries />} />
+                    <Route index element={<Home />} />
                     <Route path="countries/:id" element={<Country />} />
                     <Route path="*" element={<ErrorPage />} />
                 </Route>
             </Routes>
         </BrowserRouter>
-    );
+    )
 }
