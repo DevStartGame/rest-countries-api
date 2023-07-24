effad62ac372d3fecb1632114a55f0b5fe026651
derciojds <derciojdsd@gmail.com>
2023-07-24 17:32:59 +0200

    refactor: Alterar nome do dir. nav-bar para NavBar

--- src/layouts/GlobalLayout.jsx
+++ src/layouts/GlobalLayout.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from 'react-router-dom'
-import { NavBar } from '../components/nav-bar'
+import { NavBar } from '../components/NavBar'
 
 export default function GlobalLayout() {
     return (
