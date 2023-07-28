431605942ef4c8704bf1e1744416ad4c9c008ff2
Erivelton da Silva <eriveltondasilva13@gmail.com>
2023-07-28 19:39:07 -0300

    add da função getCountry()

--- src/services/api.js
+++ src/services/api.js
@@ -1,14 +1,24 @@
 import axios from 'axios'
 
 /*
- * @param setCountries - hook(useState) que conterá todos os dados dos países
+ * @param setCountries - hook(useState) que guardará todos os dados dos países da página Home
  */
 export async function getCountries(setCountries) {
-    // TODO: colocar toda a lógica nesta função
     const URL = 'https://restcountries.com/v3.1/all'
     const data = await getData(URL)
 
     return setCountries(data)
+}
+
+/*
+ * @param setCountry - hook(useState) que guardará todos os dados dos país da página Details
+ * @param {string} name - nome do país q virá da função "useParams()" do react router
+ */
+export async function getCountry(setCountry, name) {
+    const URL = `https://restcountries.com/v3.1/name/${name}`
+    const data = await getData(URL)
+
+    return setCountry(data)
 }
 
 function getData(URL) {
