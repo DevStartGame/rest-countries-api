7de613825f9703a4977ed46e17594688c6a6228b
Erivelton da Silva <eriveltondasilva13@gmail.com>
2023-07-30 16:46:45 -0300

    criação do slug

--- src/components/CountryCard/index.jsx
+++ src/components/CountryCard/index.jsx
@@ -8,16 +8,18 @@ const Info = ({ label, value }) => (
 )
 
 export default function CountryCard({ name, flag, region, population, capital }) {
+    const slug = name.replace(/\s/g, '-')
+
     return (
         <div className={styles['country-card']}>
-            <Link to={`/country-details/${name}`}>
-            <figure style={{ '--bgFlag': `url("${flag}")` }}></figure>
-            <div className={styles['card-body']}>
-                <h3>{name}</h3>
-                <Info label="Population" value={population} />
-                <Info label="Region" value={region} />
-                <Info label="Capital" value={capital} />
-            </div>
+            <Link to={`/country-details/${slug}`}>
+                <figure style={{ '--bgFlag': `url("${flag}")` }}></figure>
+                <div className={styles['card-body']}>
+                    <h3>{name}</h3>
+                    <Info label="Population" value={population} />
+                    <Info label="Region" value={region} />
+                    <Info label="Capital" value={capital} />
+                </div>
             </Link>
         </div>
     )
